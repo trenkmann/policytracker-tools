74e5be713c4e5c1fc63d266e82b13695f2490fd9
trenkmann <maximilian.trenkmann@gmail.com>
2021-03-04 13:20:32 +0100

    Add Ability to get Country Name from Code

--- src/components/CountryFinder.vue
+++ src/components/CountryFinder.vue
@@ -1,13 +1,26 @@
 <template>
   <section class="countryfinder">
+    <h3>Country Name to Code</h3>
     <el-input
-      v-model="input"
+      v-model="inputFromName"
       type="textarea"
       :rows="2"
       placeholder="Enter text here"
     />
     <el-input
-      v-model="found"
+      v-model="foundFromName"
+      type="textarea"
+      :rows="2"
+    />
+    <h3>Country Code to Name</h3>
+    <el-input
+      v-model="inputFromCode"
+      type="textarea"
+      :rows="2"
+      placeholder="Enter text here"
+    />
+    <el-input
+      v-model="foundFromCode"
       type="textarea"
       :rows="2"
     />
@@ -24,24 +37,43 @@ export default {
   name: 'CountryFinder',
   data() {
     return {
-      input: 'greece Greece Austira austria',
+      inputFromName: 'greece Greece Austira austria',
+      inputFromCode: 'Aut aut AUT GRC grc ; GrC',
     };
   },
   computed: {
-    lowercaseInput() {
-      return this.input?.toLowerCase();
-    },
-    found() {
+    foundFromName() {
       const cNames = Object.values(countries.getNames('en')).flat();
 
+      const input = this.inputFromName?.toLowerCase();
+
       const resultsNames = cNames.filter(
-        (country) => this.lowercaseInput.includes(country.toLowerCase()),
+        (country) => input.includes(country.toLowerCase()),
       );
 
       const results = resultsNames.map((c) => countries.getAlpha3Code(c, 'en'));
 
       return [...results].join('; ');
     },
+    foundFromCode() {
+      const cCodes = Object.keys(countries.getAlpha3Codes('en')).flat();
+
+      const input = this.inputFromCode?.toUpperCase();
+
+      const resultsCodes = cCodes.filter(
+        (country) => input.includes(country.toUpperCase()),
+      );
+
+      const results = resultsCodes.map((c) => countries.getName(c, 'en'));
+
+      return [...results].join('; ');
+    },
   },
 };
 </script>
+<style lang="scss">
+.countryfinder{
+  flex: 1 0 300px;
+  max-width: 500px;
+}
+</style>
